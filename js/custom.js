e44f1579e808faa1e685b2b7d4182b6d2b5d64ee
sebastianburs <77584718+sebastianburs@users.noreply.github.com>
2021-04-03 21:09:51 +0200

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,165 @@
+// JavaScript Document
+
+$(window).load(function () {
+    "use strict";
+    // makes sure the whole site is loaded
+    $('#status').fadeOut(); // will first fade out the loading animation
+    $('#preloader').delay(350).fadeOut('slow'); // will fade out the white DIV that covers the website.
+    $('body').delay(350).css({
+        'overflow': 'visible'
+    });
+})
+
+$(document).ready(function () {
+    "use strict";
+
+    // scroll menu
+    var sections = $('.section'),
+        nav = $('.navbar-fixed-top,footer'),
+        nav_height = nav.outerHeight();
+
+    $(window).on('scroll', function () {
+        var cur_pos = $(this).scrollTop();
+
+        sections.each(function () {
+            var top = $(this).offset().top - nav_height,
+                bottom = top + $(this).outerHeight();
+
+            if (cur_pos >= top && cur_pos <= bottom) {
+                nav.find('a').removeClass('active');
+                sections.removeClass('active');
+
+                $(this).addClass('active');
+                nav.find('a[href="#' + $(this).attr('id') + '"]').addClass('active');
+            }
+        });
+    });
+
+    nav.find('a').on('click', function () {
+        var $el = $(this),
+            id = $el.attr('href');
+
+        $('html, body').animate({
+            scrollTop: $(id).offset().top - nav_height + 2
+        }, 600);
+
+        return false;
+    });
+
+
+    // Menu opacity
+    if ($(window).scrollTop() > 80) {
+        $(".navbar-fixed-top").addClass("bg-nav");
+    } else {
+        $(".navbar-fixed-top").removeClass("bg-nav");
+    }
+    $(window).scroll(function () {
+        if ($(window).scrollTop() > 80) {
+            $(".navbar-fixed-top").addClass("bg-nav");
+        } else {
+            $(".navbar-fixed-top").removeClass("bg-nav");
+        }
+    });
+
+
+
+    // Parallax
+    var parallax = function () {
+        $(window).stellar();
+    };
+
+    $(function () {
+        parallax();
+    });
+
+    // AOS
+    AOS.init({
+        duration: 1200,
+        once: true,
+        disable: 'mobile'
+    });
+
+    //  isotope
+    $('#projects').waitForImages(function () {
+        var $container = $('.portfolio_container');
+        $container.isotope({
+            filter: '*',
+        });
+
+        $('.portfolio_filter a').click(function () {
+            $('.portfolio_filter .active').removeClass('active');
+            $(this).addClass('active');
+
+            var selector = $(this).attr('data-filter');
+            $container.isotope({
+                filter: selector,
+                animationOptions: {
+                    duration: 500,
+                    animationEngine: "jquery"
+                }
+            });
+            return false;
+        });
+
+    });
+
+    //animatedModal
+    $("#demo001,#demo002,#demo003,#demo004,#demo005,#demo006,#demo007,#demo008,#demo009,#demo010,#demo011,#demo012,#demo013,#demo014,#demo015,#demo016,#demo017,#demo018,#demo019,#demo020,#demo021,#demo022,#demo023,#demo024,#demo025,#demo026,#demo027,#demo028,#demo029,#demo030,#demo031,#demo032,#demo033,#demo034,#demo035,#demo036,#demo037,#demo038,#demo039,#demo040,#demo041,#demo042,#demo043,#demo044,#demo045,#demo046,#demo047,#demo048,#demo048,#demo049,#demo050,#demo051,#demo052,#demo053,#demo054,#demo055,#demo056,#demo057,#demo058,#demo059,#demo060,#demo061,#demo062,#demo063,#demo064,#demo065,#demo066,#demo067,#demo068,#demo069,#demo070,#demo071,#demo072,#demo073,#demo074,#demo075,#demo076,#demo077,#demo078,#demo078,#demo079,#demo080,#demo081,#demo082,#demo083,#demo084,#demo085,#demo086,#demo087,#demo088,#demo089,#demo090,#demo091,#demo092,#demo093,#demo094,#demo095,#demo096,#demo097,#demo098,#demo099,#demo100,#demo101,#demo102,#demo103,#demo104,#demo105,#demo106,#demo107,#demo108,#demo109,#demo110,#demo111,#demo112,#demo113,#demo114,#demo115,#demo116,#demo117,#demo118,#demo119").animatedModal();
+
+    // Contact Form 	
+
+    // validate contact form
+    $(function () {
+        $('#contact-form').validate({
+            rules: {
+                name: {
+                    required: true,
+                    minlength: 2
+                },
+                email: {
+                    required: true
+                },
+                phone: {
+                    required: false
+                },
+                message: {
+                    required: true
+                }
+
+            },
+            messages: {
+                name: {
+                    required: "This field is required",
+                    minlength: "your name must consist of at least 2 characters"
+                },
+                email: {
+                    required: "This field is required"
+                },
+                message: {
+                    required: "This field is required"
+                }
+            },
+            submitHandler: function (form) {
+                $(form).ajaxSubmit({
+                    type: "POST",
+                    data: $(form).serialize(),
+                    url: "process.php",
+                    success: function () {
+                        $('#contact :input').attr('disabled', 'disabled');
+                        $('#contact').fadeTo("slow", 1, function () {
+                            $(this).find(':input').attr('disabled', 'disabled');
+                            $(this).find('label').css('cursor', 'default');
+                            $('#success').fadeIn();
+                        });
+                    },
+                    error: function () {
+                        $('#contact').fadeTo("slow", 1, function () {
+                            $('#error').fadeIn();
+                        });
+                    }
+                });
+            }
+        });
+
+    });
+});
